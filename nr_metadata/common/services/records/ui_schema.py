aaad8741aa331abf4f68a66d0ec5edb43b5efcbe
Mirek Simek <miroslav.simek@gmail.com>
2023-03-24 16:12:10 +0100

    Localized subject field

--- nr_metadata/common/services/records/ui_schema.py
+++ nr_metadata/common/services/records/ui_schema.py
@@ -1,6 +1,10 @@
 import marshmallow as ma
 from marshmallow import fields as ma_fields
-from oarepo_runtime.i18n.ui_schema import I18nStrUIField, MultilingualUIField
+from oarepo_runtime.i18n.ui_schema import (
+    I18nStrUIField,
+    MultilingualLocalizedUIField,
+    MultilingualUIField,
+)
 from oarepo_runtime.ui import marshmallow as l10n
 from oarepo_vocabularies.services.ui_schemas import HierarchyUISchema, I18nStrUIField
 
@@ -74,7 +78,7 @@ class NRSubjectUISchema(ma.Schema):
     """NRSubjectUISchema schema."""
 
     subjectScheme = ma_fields.String()
-    subject = MultilingualUIField(I18nStrUIField())
+    subject = MultilingualLocalizedUIField(I18nStrUIField())
     valueURI = ma_fields.String()
     classificationCode = ma_fields.String()
 
